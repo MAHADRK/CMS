a4236364208f52492a0a99b2e8988d2c1bb59aec
MAHADRK <mahadraza79@gmail.com>
2022-08-16 15:44:44 +0200

    uploading updated file dbclass and filetoinputata

--- dbclass.py
+++ dbclass.py
@@ -2,27 +2,40 @@ import sqlite3
 con = sqlite3.connect('CMS.db')
 cur = con.cursor()
 
-# *******************************TO DROP TABLE PLZ UNCOMMENT***********************************************
+# *******************************TO DROP TABLE Please UNCOMMENT***********************************************
 # cur.execute(''' DROP TABLE catagory''')
 # cur.execute(''' DROP TABLE subcatagory''')
 # cur.execute(''' DROP TABLE description''')
 # cur.execute('''DROP TABLE cashflow''')
 
+# Create table for catagory
+#  using ID's to implement relational data base
+
 cur.execute('''CREATE TABLE catagory
         ( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
         nameCat VARCHAR UNIQUE)''')
+
+# create table for subctagory
+#  using ID to implement relational data base
+
 cur.execute('''CREATE TABLE subcatagory
         (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
         catagory_id VARCHAR,
         namesubCat VARCHAR UNIQUE)''')
+
+# create table for description
+# using ID's to implement relational data base
 
 cur.execute('''CREATE TABLE description 
         (voucher_no INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
         catagory_id INTEGER ,
         subcatagory_id VARCHAR,
         description VARCHAR, amount INTEGER, inputdate TEXT )''')
+
+# create table for cashflow with column CashIN and DOI date of input cash
+
 cur.execute('''CREATE TABLE cashflow 
-        (OB INTEGER ,
+        (CashIN INTEGER ,
         DOI TEXT )''')
 
 # Save (commit) the changes
--- filetoinputdata.py
+++ filetoinputdata.py
@@ -1,219 +1,161 @@
-print('Bismillah','\n', 'test')
+print('Bismillah', '\n', 'testing')
 import sqlite3
+
 conn = sqlite3.connect('CMS.db')
 cur = conn.cursor()
 from datetime import date
 
+
+# a parent class will deal inputs to the table
 class CashManagement:
 
-    def __init__(self,catagory, subcatagory, descrip,amt,inputdate):
-        self.catagory = catagory
-        self.subcatagory = subcatagory
+    def __init__(self, category, subcategory, descrip, amt, inputdate):
+        # initialize instances for class
+        self.category = category
+        self.subcategory = subcategory
         self.descrip = descrip
         self.amt = amt
         self.inputdate = inputdate
 
-
-
-
-    def inputData_catagory(self):
+    # creating method to input data to category
+    def inputData_category(self):
         with conn:
-            cur.execute('''INSERT OR IGNORE INTO catagory (nameCat) VALUES (?)  ''',(self.catagory,))
+            cur.execute('''INSERT OR IGNORE INTO category (nameCat) VALUES (?)  ''', (self.category,))
             # INSERT OR IGNORE
-            # cur.execute("INSERT or IGNORE INTO catagory VALUES (:id,:catagory) ", {'id': None, 'catagory': self.catagory})
-             # cur.execute("INSERT INTO catagory VALUES (:id,:catagory) ", {'id': None, 'catagory': self.catagory})
+            # cur.execute("INSERT or IGNORE INTO category VALUES (:id,:category) ", {'id': None, 'category': self.category})
+            # cur.execute("INSERT INTO category VALUES (:id,:category) ", {'id': None, 'category': self.category})
 
-
-
-    def fatch_catagoryid(self):
+    # method to fetch category id via its name to use this category id in description table and subcategory table
+    def fetch_categoryid(self):
         with conn:
-            cur.execute("SELECT id FROM catagory WHERE nameCat = ? ", (self.catagory, ))
-            # IdCatagoryCatagory =
+            cur.execute("SELECT id FROM category WHERE nameCat = ? ", (self.category,))
         return cur.fetchone()[0]
 
-
-
-
-
-    def inputdata_subcatagory(self,catagory_id):
+    # method to input category_id and subcategory name to the subcategory table
+    def inputdata_subcategory(self, category_id):
         with conn:
-            cur.execute('''INSERT OR IGNORE INTO subcatagory (catagory_id, namesubCat) VALUES (? ,?)  ''',(catagory_id,self.subcatagory))
+            cur.execute('''INSERT OR IGNORE INTO subcategory (category_id, namesubCat) VALUES (? ,?)  ''',
+                        (category_id, self.subcategory))
 
-
-
-
-    def fatch_subcatagoryid(self):
+    # method to fetch subcategory id to save the id to description table
+    def fetch_subcategoryid(self):
         with conn:
-            cur.execute("SELECT id FROM subcatagory WHERE namesubCat = ? ", (self.subcatagory,))
-            # IdCatagoryCatagory =
+            cur.execute("SELECT id FROM subcategory WHERE namesubCat = ? ", (self.subcategory,))
             return cur.fetchone()[0]
 
-
-
-
-    def inputdata_description(self,catagory_id,subcatagory_id):
+    # input category id and subcategory id to description table
+    def inputdata_description(self, category_id, subcategory_id):
         with conn:
-            cur.execute('''INSERT OR IGNORE INTO description (catagory_id,subcatagory_id, description, amount,inputdate) VALUES (?,? ,?, ?,?)  ''',
-                        (catagory_id,subcatagory_id, self.descrip, self.amt,self.inputdate))
+            cur.execute(
+                '''INSERT OR IGNORE INTO description (category_id,subcategory_id, description, amount,inputdate) VALUES (?,? ,?, ?,?)  ''',
+                (category_id, subcategory_id, self.descrip, self.amt, self.inputdate))
 
-
-
+    # taking sum of amount in description table
     def addamount(self):
         with conn:
             cur.execute('''SELECT sum(amount) FROM description''')
             value = cur.fetchone()
-            print('amount from function',value)
+            print('amount from function', value)
         return value
 
-
-
+    # this method to call all the methods and input all the data to respective tables
     def inputcompletedata(self):
-        CashManagement.inputData_catagory(self)
-        catagory_id = CashManagement.fatch_catagoryid(self)
-        CashManagement.inputdata_subcatagory(self,catagory_id)
-        subcatagory_id = CashManagement.fatch_subcatagoryid(self)
-        CashManagement.inputdata_description(self,catagory_id,subcatagory_id)
+        CashManagement.inputData_category(self)
+        category_id = CashManagement.fetch_categoryid(self)
+        CashManagement.inputdata_subcategory(self, category_id)
+        subcategory_id = CashManagement.fetch_subcategoryid(self)
+        CashManagement.inputdata_description(self, category_id, subcategory_id)
         # FinalData = CashManagement.retrievecompletedata(self)
         # return FinalData
 
-
+    # method to retrieve data from table
     def retrievecompletedata(self):
-        catid = CashManagement.fatch_catagoryid(self)
-        subcatid = CashManagement.fatch_subcatagoryid(self)
+        # selecting variables to show main window, Connecting table with JOIN to fetch values
         with conn:
-            # cur.execute('''SELECT catagory.nameCat,   subcatagory.namesubCat
-            # FROM catagory JOIN subcatagory on catagory.id = :CAT and subcatagory.catagory_id = :subCat''', {'CAT': 4, 'subCat': 6})
-            cur.execute('''SELECT voucher_no, catagory.nameCat,   subcatagory.namesubCat, description.description ,description.amount,description.inputdate 
-            FROM catagory JOIN subcatagory JOIN description on catagory.id = subcatagory.catagory_id and subcatagory.id = description.subcatagory_id''')
+            cur.execute('''SELECT voucher_no, category.nameCat, subcategory.namesubCat, description.description ,description.amount,description.inputdate 
+            FROM category JOIN subcategory JOIN description on category.id = subcategory.category_id and subcategory.id = description.subcategory_id''')
             data = cur.fetchall()
-            print('data',data)
+            print('data', data)
+            # sum amount to get total amount
             cur.execute('''SELECT sum(description.amount) 
-                            FROM catagory 
-                            JOIN subcatagory 
+                            FROM category 
+                            JOIN subcategory 
                             JOIN description 
-                            on catagory.id = subcatagory.catagory_id 
-                            and subcatagory.id = description.subcatagory_id''')
-                        #      WHERE catagory.id = :catid and subcatagory.id = :subcatid 
-                        #        and description.catagory_id = :catid and description.subcatagory_id = :subcatid ''',
-                        # {'catid': catid, 'subcatid': subcatid})
+                            on category.id = subcategory.category_id 
+                            and subcategory.id = description.subcategory_id''')
             sumamount = cur.fetchall()
-        return data,sumamount
-    def retrievecompletedatabymonth(self,startdate,enddate):
+        return data, sumamount
+
+    # method to retrieve data by any month
+    def retrievecompletedatabymonth(self, startdate, enddate):
+        # BETWEEN is creating a range of dates of user input
         with conn:
-            cur.execute('''SELECT description.voucher_no,catagory.nameCat, subcatagory.namesubCat, description.description, description.amount, description.inputdate
-                       FROM catagory JOIN subcatagory JOIN description ON catagory.id = subcatagory.catagory_id and subcatagory.id = description.subcatagory_id
+            cur.execute('''SELECT description.voucher_no,category.nameCat, subcategory.namesubCat, description.description, description.amount, description.inputdate
+                       FROM category JOIN subcategory JOIN description ON category.id = subcategory.category_id and subcategory.id = description.subcategory_id
                          WHERE (inputdate BETWEEN :startdate AND :enddate)''',
                         {'startdate': startdate, 'enddate': enddate})
             data = cur.fetchall()
-            cur.execute('''SELECT sum(description.amount) FROM catagory JOIN subcatagory JOIN description ON catagory.id = subcatagory.catagory_id and subcatagory.id = description.subcatagory_id
+            # fetching total amount
+            cur.execute('''SELECT sum(description.amount) FROM category JOIN subcategory JOIN description ON category.id = subcategory.category_id and subcategory.id = description.subcategory_id
                          WHERE (inputdate BETWEEN :startdate AND :enddate) ''',
                         {'startdate': startdate, 'enddate': enddate})
             amount = cur.fetchall()
-            return data,amount
-# **********************************************************CHILD CLASSS TO RETRIEVE BY CATAGORY********************************************************************************
+            return data, amount
 
 
-class Fetchbycatagory(CashManagement):
-    def __init__(self,catagory,subcatagory,startdate,enddate):
-        self.catagory = catagory
-        self.subcatagory = subcatagory
+# **********************************************************CHILD CLASSS TO RETRIEVE BY category********************************************************************************
+# this class was separate to make the code more modular and easier to understand
+
+class Fetchbycategory(CashManagement):
+    def __init__(self, category, subcategory, startdate, enddate):
+        # initialize instances for child class
+        self.category = category
+        self.subcategory = subcategory
         self.startdate = startdate
         self.enddate = enddate
 
-
-    def retrievebycatagory(self):
-        catid = CashManagement.fatch_catagoryid(self)
-        subcatid = CashManagement.fatch_subcatagoryid(self)
-        print(self.catagory,self.subcatagory,self.startdate,self.enddate)
+    # method to retrieve data by category
+    def retrievebycategory(self):
+        # fetch category and subcategory id of user inputs
+        catid = CashManagement.fetch_categoryid(self)
+        subcatid = CashManagement.fetch_subcategoryid(self)
+        print("-----",catid,subcatid)
+        # print(self.category, self.subcategory, self.startdate, self.enddate)
+        # Query to connect tables on the basis category id and subcategory id and date
         with conn:
-            cur.execute('''SELECT description.voucher_no,catagory.nameCat, subcatagory.namesubCat, description.description, description.amount, description.inputdate
-                FROM catagory JOIN subcatagory JOIN description ON catagory.id = subcatagory.catagory_id
-                  WHERE catagory.id = :catid and subcatagory.id = :subcatid
-                    and description.catagory_id = :catid and description.subcatagory_id = :subcatid and (inputdate BETWEEN :startdate AND :enddate)''',
-                        {'catid': catid, 'subcatid': subcatid, 'startdate': self.startdate,'enddate':self.enddate})
+            cur.execute('''SELECT description.voucher_no,category.nameCat, subcategory.namesubCat, description.description, description.amount, description.inputdate
+                FROM category JOIN subcategory JOIN description ON category.id = subcategory.category_id
+                  WHERE category.id = :catid and subcategory.id = :subcatid
+                    and description.category_id = :catid and description.subcategory_id = :subcatid and (inputdate BETWEEN :startdate AND :enddate)''',
+                        {'catid': catid, 'subcatid': subcatid, 'startdate': self.startdate, 'enddate': self.enddate})
             data = cur.fetchall()
+            # Query to fetching total amount
             cur.execute('''SELECT sum(description.amount)
-                       FROM catagory JOIN subcatagory JOIN description ON catagory.id = subcatagory.catagory_id  
-                         WHERE catagory.id = :catid and subcatagory.id = :subcatid 
-                           and description.catagory_id = :catid and description.subcatagory_id = :subcatid and (inputdate BETWEEN :startdate AND :enddate)''',
-                    {'catid': catid, 'subcatid': subcatid, 'startdate': self.startdate, 'enddate': self.enddate})
+                       FROM category JOIN subcategory JOIN description ON category.id = subcategory.category_id  
+                         WHERE category.id = :catid and subcategory.id = :subcatid 
+                           and description.category_id = :catid and description.subcategory_id = :subcatid and (inputdate BETWEEN :startdate AND :enddate)''',
+                        {'catid': catid, 'subcatid': subcatid, 'startdate': self.startdate, 'enddate': self.enddate})
             sumamount = cur.fetchall()
             return sumamount, data
-    # def catagorydatacall(self):
-    #     catagory_id = CashManagement.fatch_catagoryid(self)
-    #     subcatagory_id = CashManagement.fatch_subcatagoryid(self)
-    #     print("retrived records to print")
-    #     value = CashManagement.fetchtoextrct(self)
-    #     return value
-    def inputcashflowdata(self,OB,date):
-        print(OB,'date',date)
-        with conn:
-            cur.execute(''' INSERT into cashflow (OB,DOI) VALUES(?,?)''',(OB,date))
 
+    # to take cash which is added to account
+    def inputcashflowdata(self, CashIN, date):
+        print("input cash", CashIN, 'date', date)
+        #  Query to inset user input cash to the db
+        with conn:
+            cur.execute(''' INSERT into cashflow (CashIN,DOI) VALUES(?,?)''', (CashIN, date))
+
+    # to fetch out cash from database
     def retrievecashflowdata(self):
+        # query tos sum cash in the db and fetch out
         with conn:
-            cur.execute('''SELECT sum(OB) from cashflow''')
+            cur.execute('''SELECT sum(CashIN) from cashflow''')
         return cur.fetchall()
-    def retrievecashflowdatacl(self,strtdate,endate):
+
+    # fetch input cash from particular dates
+    def retrievecashflowdatacl(self, strtdate, endate):
         with conn:
-            cur.execute('''SELECT OB,DOI from cashflow WHERE (DOI BETWEEN :startdate AND :enddate) ''',{'startdate': strtdate, 'enddate': endate})
+            cur.execute('''SELECT CashIN,DOI from cashflow WHERE (DOI BETWEEN :startdate AND :enddate) ''',
+                        {'startdate': strtdate, 'enddate': endate})
         return cur.fetchall()
-# *************************************************************  objects     ***************************************************
 
-
-# data1 = CashManagement('Maintainance','paint','quarter paint 2 bucket',20000,'2021-10-16')
-# data2 = CashManagement('Food','Lunch','Salan roti',900,'2021-10-16')
-# data3 = CashManagement('Project','Zaman pvt ltd','Smokes',20000,'2021-11-16')
-# data4 = CashManagement('Project','Rajby','Cables',9000,'2021-10-15')
-# data5 = CashManagement('Project','Emaar','Inin Compact 2 loop',40000, '2021-09-17')
-# data6 = CashManagement('Project', 'Avt', 'Smart loop', 60000, '2021-05-12')
-# data7 = CashManagement('Project','Emaar','Cables 2mm',80000, '2021-06-12')
-# data8 = CashManagement('Project','Emaar','smoke',55000,'2021-07-16')
-# data9 = CashManagement('Project','Emaar','heat detectors',70000,'2021-10-09')
-# data10 = CashManagement('Lunch','Aljibra house','sounder with flasher',60000,'2021-09-12')
-# data11 = CashManagement('Project','Emaar','sounder with flasher',60000,'2021-09-12')
-# list_data[circle], list_data[circle+1], list_data[circle+2],list_data[circle+3],list_data[circle+4
-# colldata = [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,data11]
-# # ********************************callclassfunctions*******************************
-# for data in colldata:
-#     CashManagement.inputcompletedata(data)
-# dumydata = CashManagement('test', 'test', 'test', 0, '2021-10-27')
-
-
-
-
-# %%%%%%%%%%%%%%%%%%%%%%%% MOnthly %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# month = date.today().month
-# year= date.today().year
-# monthstart = "{yy}-{mm}-{dd}".format(yy=year, mm = month, dd = '1')
-# monthend = "{yy}-{mm}-{dd}".format(yy=year, mm = month, dd = '31')
-# print(monthend,monthstart)
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%% all data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# d,a = CashManagement.retrievecompletedatabymonth(None,monthstart,monthend)
-# print('dataaa',d,'amountt',a)
-# da,am = CashManagement.retrievecompletedata(dumydata)
-# print('datacom',da,'compleamount',am)
-
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# print('input complete data',CashManagement.inputcompletedata(dumydata))
-# data = Fetchbycatagory('Lunch','Aljibra house','2021-09-12','2021-09-20')
-# print(CashManagement.retrievecompletedata(dumydata))
-# print('catagory',Fetchbycatagory.retrievebycatagory(data))
-#
-# amt,data = CashManagement.retrievecompletedata(data11)
-# Fetchbycatagory.inputcashflowdata(None,'30','3213')
-# print(data)
-# print(amnt[0][0])
-# data = Fetchbycatagory('Project', 'Emaar','2021-05-12','2021-11-16')
-# data = Fetchbycatagory('test', 'test','2021-10-27','2021-10-28')
-# sumamt, catagorywisedata = Fetchbycatagory.retrievebycatagory(data)
-# print('all', CashManagement.retrievecompletedata(data11))
-# print(catagorywisedata)
-# for data in catagorywisedata:
-#     print(data)
-# amount = list(Fetchbycatagory.addamount(data))
-# print('total amount: ',amount)
